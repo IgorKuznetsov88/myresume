192790f51656b252230f68e49687db80c0108c20
Kuznetsov Igor <igorkuznetsov@me.com>
2019-10-09 13:32:26 +0400

    Updates

---
+++ precache-manifest.11545b42f2e3b5abd2d54a63638ee06e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8533be8253257116961b6569336acc",
+    "url": "/myresume/index.html"
+  },
+  {
+    "revision": "1a95d756a2091204a559",
+    "url": "/myresume/static/css/2.e44fad29.chunk.css"
+  },
+  {
+    "revision": "bdeb8c5616badb84e524",
+    "url": "/myresume/static/css/main.2da5157b.chunk.css"
+  },
+  {
+    "revision": "1a95d756a2091204a559",
+    "url": "/myresume/static/js/2.f8c063ba.chunk.js"
+  },
+  {
+    "revision": "bdeb8c5616badb84e524",
+    "url": "/myresume/static/js/main.25bea8f8.chunk.js"
+  },
+  {
+    "revision": "14d3063d35f6710507c4",
+    "url": "/myresume/static/js/runtime-main.68b8f3dd.js"
+  },
+  {
+    "revision": "cd490bd0b9243ac66a2f5e3a899015d8",
+    "url": "/myresume/static/media/background.cd490bd0.jpg"
+  },
+  {
+    "revision": "0365de6b09684ee323770be48073f028",
+    "url": "/myresume/static/media/css_sertificate.0365de6b.JPG"
+  },
+  {
+    "revision": "3863310c85fd0ade387b74ca8431c6ab",
+    "url": "/myresume/static/media/html_sertificate.3863310c.JPG"
+  },
+  {
+    "revision": "33345e2102036483b8e68b40af1fc3ce",
+    "url": "/myresume/static/media/js_sertificate.33345e21.JPG"
+  },
+  {
+    "revision": "84da4cf6c0ef95b61ac5905d052505f6",
+    "url": "/myresume/static/media/my_photo.84da4cf6.JPG"
+  }
+]);
