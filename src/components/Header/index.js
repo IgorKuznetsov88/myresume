fa88d44c45f4eba32d2bd0152f2136ab3d992e2d
Kuznetsov Igor <igorkuznetsov@me.com>
2019-10-09 14:03:02 +0400

    Update card

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -16,7 +16,7 @@ class Header extends Component {
       <header style={myBackground} className="background-image">
         <h1>
           <p>Игорь </p>
-          <span className="surname"> К </span>
+          <span className="surname"> Кузнецов </span>
         </h1>
         
       </header>
